71687d43ff0a1d10d7e42d08e4d8d9c66fa63e28
CristhianMotoche <cristhian.motoche@gmail.com>
2021-03-23 18:39:10 -0500

    More scripts!

---
+++ basics/array.R
@@ -0,0 +1,6 @@
+# Multi dimensional array
+ar1 = array(data=1:8, dim=c(2,2,2))
+ar1
+
+dim(ar1)
+ar1[1,2,1]
---
+++ basics/functions.R
@@ -0,0 +1,26 @@
+getSum = function(n1, n2) {
+  n1+n2
+}
+
+getSum(3,5)
+
+missArg = function(x) {
+  if(missing(x)) {
+    return("Missing arg")
+  } else {
+    return(x)
+  }
+}
+
+missArg()
+missArg(3)
+
+getMany = function(...) {
+  for(i in list(...)) {
+    print(i)
+  }
+}
+
+getMany(1,2,3,4,4:10)
+
+(function (x) x * 2)(10)
