2e5c6d25004ea52e82688840ec3acd40a5874bd8
CristhianMotoche <cristhian.motoche@gmail.com>
2021-03-26 17:14:30 -0500

    Ploting scripts and some operations

---
+++ basics/ops.R
@@ -0,0 +1,23 @@
+sqrt(100)
+
+log(1)
+
+ns = c(1:20)
+
+sum(ns)
+
+mean(ns)
+
+median(ns)
+
+max(ns)
+min(ns)
+range(ns)
+
+floor(3.5)
+ceiling(3.5)
+
+cumsum(ns)
+cumprod(ns)
+cummax(c(4:7, 1:10, 10:20))
+cummin(c(4:7, 1:10, 10:20))
--- basics/ploting.R
+++ basics/ploting.R
@@ -1,3 +1,4 @@
+# Plot lines and dots
 xy1 = matrix(
   data=c(1, 2, 3, 4, 5, 1, 2, 3, 4, 5),
   nrow=5,
@@ -17,4 +18,18 @@ plot(
    xlim=c(-1,8),
    ylim=c(-1,8),
 )
+
 abline(h=c(2,4), col="red", lty=2)
+
+segments(x0=c(2, 4), y0=c(2, 2), x1=c(2, 4), y1=c(4, 4), col="red", lty=2)
+ 
+arrows(x0=1.5, y0=4.5, x1=2.7, y1=3.25, col="green")
+
+text(x=1.25, y=4.74, labels="Center")
+
+# Scatter plots
+
+plot(faithful)
+
+erups = with(faithful, faithful[eruptions > 4,])
+points(erups, col="red", pch=3)
